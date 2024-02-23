a07a676614ad6866047e9b4f3497deded7b7ec4e
Ernesto dev-stream <ernesto.dev.stream@gmail.com>
2024-02-24 06:36:04 +0800

    update nvim dev-stream

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -54,3 +54,28 @@ vim.keymap.set("n", "<leader>gt", "<cmd>GoTest<CR>", { desc = "Go Test" })
 -- snippets
 
 vim.keymap.set("n", "<leader>hy", "i{{< youtube id >}}<Esc>", { desc = "Hugo Youtube" })
+
+
+-- Enter command mode
+vim.keymap.set({ "n", "v" }, ";", ":", { nowait = true })
+vim.keymap.set({ "n", "v" }, ":", ";", { nowait = true })
+
+-- Visual block mode
+vim.keymap.set("n", "<C-q>", "<C-v>", { noremap = true })
+
+-- Visually select pasted or yanked text
+vim.keymap.set("n", "gV", "`[v`]", { noremap = true })
+
+-- navigate tab completion with <c-j> and <c-k>
+-- runs conditionally
+vim.keymap.set("c", "<C-j>", function()
+  return vim.fn.pumvisible() == 1 and "<C-n>" or "<Down>"
+  end, { expr = true, noremap = true })
+vim.keymap.set("c", "<C-k>", function()
+  return vim.fn.pumvisible() == 1 and "<C-p>" or "<Up>"
+  end, { expr = true, noremap = true })
+
+-- Navigate buffers
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", { silent = true })
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { silent = true })
+
